31ab486578f94fa651744046d4c84d8d69bfb633
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-10-11 18:24:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".es"];
+
+  for (let x = 0; x < pronoun.length; x++) {
+    for (let y = 0; y < adj.length; y++) {
+      for (let z = 0; z < noun.length; z++) {
+        for (let w = 0; w < domain.length; w++) {
+          console.log(pronoun[x] + adj[y] + noun[z] + domain[w]);
+        }
+      }
+    }
+  }
 };
